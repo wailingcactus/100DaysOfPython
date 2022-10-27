2e00c9005999b0303ec4fe1cc4c154352a15e3d0
wailingcactus <76265385+wailingcactus@users.noreply.github.com>
2022-10-27 14:51:08 +0200

    Day 24 - Working with Files and Directories

--- Day 20 - Snake/scoreboard.py
+++ Day 20 - Snake/scoreboard.py
@@ -5,6 +5,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.draw_score()
 
     def draw_score(self):
@@ -13,6 +14,11 @@ class Scoreboard(Turtle):
         self.penup()
         self.setposition(0, 270)
         self.write(f"Score: {self.score}", move=False, align='center', font=('Calibri', 11, 'normal'))
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
 
     def game_over(self):
         self.setposition(0, 0)
---
+++ Day 24 - Adding High Score/File Test/file_main.py
@@ -0,0 +1,3 @@
+with open("../../../../../Desktop/my_file.txt") as file:
+    contents = file.read()
+    print(contents)
